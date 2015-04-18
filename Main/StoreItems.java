e554df80ee0116584071fdfba06c41f88c7c8782
TekCastElemental <Max560gaming@gmail.com>
2015-04-18 16:46:10 -0500

    Release
    OSC (Open Source Concept) for a store checkout system.

---
+++ Main/StoreItems.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storeconcept;
+
+/**
+ *
+ * @author Max
+ */
+class StoreItems {
+    public static double Item1 = 3.99;
+    public static double Item2 = 2.99;
+}
